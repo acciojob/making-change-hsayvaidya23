07c34f688dbb86d6f9888a82a165708a955c809b
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-05 17:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const makeChange = (c) => {
   // your name here
+	let cents = parseInt(c);
+	const change = { q: 0, d: 0, n: 0, p: 0};
+
+	change.q = Math.floor(cents/25);
+	cents %= 25;
+	change.d = Math.floor(cents/10);
+	cents %= 10;
+	change.n = Math.floor(cents/5);
+	cents %= 5;
+	change.p = cents;
+	
+	return change;
 };
 
 // Do not the change the code below
